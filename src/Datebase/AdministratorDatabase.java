2bbc43e1a019a62a6901acc4b9d57912b292c4e5
Ahmed Ezzat <egyahmed.ezzat120@gmail.com>
2020-12-28 22:08:24 +0200

    Adding addAdministrator function and two functions about total collected

--- src/Datebase/AdministratorDatabase.java
+++ src/Datebase/AdministratorDatabase.java
@@ -1,6 +1,84 @@
 package Datebase;
 import java.sql.*;
+import Datebase.OldCustomerDatabase;
 
 public class AdministratorDatabase {
+
+
+    public static void collectPayments(double moneyValue){
+        
+        try(   
+            Connection connect = DbConnection.connect();
+            PreparedStatement ps = connect.prepareStatement("update TotalCollected set TotalColl = TotalColl + ?");
+        ){
+
+            ps.setDouble(1, moneyValue);
+
+            ps.executeUpdate();
+          
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+
+    
+    public static double viewTotalCollected(){
+        
+        try(   
+            Connection connect = DbConnection.connect();
+            PreparedStatement ps = connect.prepareStatement("select TotalColl FROM TotalCollected");
+        ){
+
+            ResultSet r = ps.executeQuery();
+            
+            return r.getDouble("TotalColl");
+          
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return 0;
+    }
+    
+    
+    public static void addAdministrator(
+            
+            String name, String nationalID, String address, String email, String phoneNumber, String gender,
+            String dateOfBirth, String administratorID, String administratorPass, String contractDate, String administratorRole
+    
+    ){
+        
+        String sqlAdd = "insert into Administrators (Name, NationalID, Address, Email, PhoneNumber, Gender, DateOfBirth, AdministratorID, AdministratorPass, ContractDate, AdministratorRole) "
+                   + "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        try(
+            Connection connect = DbConnection.connect();
+            PreparedStatement ps = connect.prepareStatement(sqlAdd);
+        ){
+            
+            ps.setString(1, name);
+            ps.setString(2, nationalID);
+            ps.setString(3, address);
+            ps.setString(4, email);
+            ps.setString(5, phoneNumber);
+            ps.setString(6, gender);
+            ps.setString(7, dateOfBirth);
+            ps.setString(8, administratorID);
+            ps.setString(9, administratorPass);
+            ps.setString(10, contractDate);
+            ps.setString(11, administratorRole);
+            
+            ps.execute();
+             
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+        
+    }
+    
+    
+    
+    
     
 }
