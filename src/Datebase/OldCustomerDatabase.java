37416bc12049ad6ce344efff1df6d89d7f5c1368
Ahmed Ezzat <egyahmed.ezzat120@gmail.com>
2020-12-28 17:30:01 +0200

    Adding some useful functions

--- src/Datebase/OldCustomerDatabase.java
+++ src/Datebase/OldCustomerDatabase.java
@@ -1,8 +1,10 @@
 package Datebase;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class OldCustomerDatabase {
     
+    // --------------------------------------------- Public functions --------------------------------------------- //
     
     public static String[] getFirstUnpaidBillInfo(String meterCode){
         
@@ -53,7 +55,6 @@ public class OldCustomerDatabase {
    
     }
     
-    
     public static void enterMonthlyReading(String meterCode, int currentReading, int tariff, double moneyValue){
         
         int pastReading = getLastReading(meterCode);
@@ -83,100 +84,16 @@ public class OldCustomerDatabase {
     }
     
     
-    
-    private static int getLastReading(String meterCode){
-        
-        try(
-            Connection connect = DbConnection.connect();
-            PreparedStatement ps = connect.prepareStatement("select CurrentReading from OldCustomer "
-                    + "where MeterCode = ? order by Num DESC limit 1");
-        ){
-            
-            ps.setString(1, meterCode);
-            ResultSet r = ps.executeQuery();
-            
-            return r.getInt("CurrentReading");
-            
-        } catch (SQLException e) {
-            System.out.println(e.toString());
-        }
-        
-        return 0;
-    }
-    
-    
-    private static String getDateOfLastBill(String meterCode){
-        
-        try(
-            Connection connect = DbConnection.connect();
-            PreparedStatement ps = connect.prepareStatement("select DateOfBill from OldCustomer "
-                    + "where MeterCode = ? order by Num DESC limit 1");
-        ){
-            
-            ps.setString(1, meterCode);
-            ResultSet r = ps.executeQuery();
-            
-            return r.getString("DateOfBill");
-            
-        } catch (SQLException e) {
-            System.out.println(e.toString());
-        }
-        
-        return "";
-    }
-    
-    
-    
-    private static String generateNewDateOfBill(String latestDate){
-        
-        int months = Integer.parseInt(latestDate.substring(0, 2));
-        int years = Integer.parseInt(latestDate.substring(4, 7));
-        
-        months = (months + 1) % 12;
-        if(months == 1)
-            years++;
-        
-        return (String.format("%02d", months) + "/" + latestDate.substring(3, 4) + String.format("%03d", years));
-    }
-    
-    
-    
     public static String getTypeOfUse(String meterCode){
         return NewCustomerDatabase.getTypeOfUseFromNC(meterCode);
     }
-    
     
     
     public static int getConsumption(String meterCode, int currentReading){
         return currentReading - getLastReading(meterCode);
     }
     
-    
-    
-    private static String getGovernmentCodeForMeter(String meterCode){
-        
-        try(
-            Connection connect = DbConnection.connect();
-            PreparedStatement ps = connect.prepareStatement("select GovernmentCode from OldCustomer "
-                    + "where MeterCode = ? order by Num DESC limit 1");
-        ){
-            
-            ps.setString(1, meterCode);
-            ResultSet r = ps.executeQuery();
-            r.next();
-            
-            return r.getString("GovernmentCode");
-            
-        } catch (SQLException e) {
-            System.out.println(e.toString());
-        }
-        
-        return "";
-    }
-    
-    
     public static int countUnpaidBills(String meterCode){
-        
         
         try(
             Connection connect = DbConnection.connect();
@@ -220,4 +137,173 @@ public class OldCustomerDatabase {
     }
     
     
+    public static String[] getLastBillInfo(String meterCode){
+        
+        String[] billInfo = new String[8];
+        
+        try(
+            Connection connect = DbConnection.connect();
+            PreparedStatement ps = connect.prepareStatement("SELECT GovernmentCode, PastReading, CurrentReading, Consumption, Tariff, MoneyValue, Status, DateOfBill FROM OldCustomer " +
+                                    "WHERE MeterCode = ? ORDER by Num DESC LIMIT 1");
+        ){
+            
+            ps.setString(1, meterCode);
+            
+            ResultSet r = ps.executeQuery();
+            r.next();
+            
+            billInfo[0] = r.getString("GovernmentCode");
+            billInfo[1] = String.valueOf(r.getInt("PastReading"));
+            billInfo[2] = String.valueOf(r.getInt("CurrentReading"));
+            billInfo[3] = String.valueOf(r.getInt("Consumption"));
+            billInfo[4] = String.valueOf(r.getInt("Tariff"));
+            billInfo[5] = String.valueOf(r.getDouble("MoneyValue"));
+            billInfo[6] = r.getString("Status");
+            billInfo[7] = r.getString("DateOfBill");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return billInfo;
+    }
+    
+    
+    public static ArrayList<String[]> getBillsInfoOfSpecificRegion(String governmentCode){
+        
+        ArrayList<String[]> billsContainer = new ArrayList<>();
+        
+        try(
+            Connection connect = DbConnection.connect();
+            PreparedStatement ps = connect.prepareStatement("SELECT MeterCode, PastReading, CurrentReading, Consumption, Tariff, MoneyValue, Status, DateOfBill FROM OldCustomer " +
+                                    "WHERE GovernmentCode = ? ORDER by Num ASC");
+        ){
+            
+            ps.setString(1, governmentCode);
+            
+            ResultSet r = ps.executeQuery();
+            
+            while(r.next()){
+            
+                String[] billInfo = new String[8];
+                billInfo[0] = r.getString("MeterCode");
+                billInfo[1] = String.valueOf(r.getInt("PastReading"));
+                billInfo[2] = String.valueOf(r.getInt("CurrentReading"));
+                billInfo[3] = String.valueOf(r.getInt("Consumption"));
+                billInfo[4] = String.valueOf(r.getInt("Tariff"));
+                billInfo[5] = String.valueOf(r.getDouble("MoneyValue"));
+                billInfo[6] = r.getString("Status");
+                billInfo[7] = r.getString("DateOfBill");
+                billsContainer.add(billInfo);
+                
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return billsContainer;
+    }
+    
+    
+    public static double getMoneyValueofLastPaidBill(String meterCode){
+        
+        try(
+            Connection connect = DbConnection.connect();
+            PreparedStatement ps = connect.prepareStatement("select MoneyValue from OldCustomer "
+                    + "where MeterCode = ? and Status = 'Paid' order by Num DESC limit 1");
+        ){
+            
+            ps.setString(1, meterCode);
+            ResultSet r = ps.executeQuery();
+            
+            return r.getDouble("MoneyValue");
+            
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        
+        return 0;
+    }
+    
+    
+    // --------------------------------------------- Private functions --------------------------------------------- //
+    
+    private static int getLastReading(String meterCode){
+        
+        try(
+            Connection connect = DbConnection.connect();
+            PreparedStatement ps = connect.prepareStatement("select CurrentReading from OldCustomer "
+                    + "where MeterCode = ? order by Num DESC limit 1");
+        ){
+            
+            ps.setString(1, meterCode);
+            ResultSet r = ps.executeQuery();
+            
+            return r.getInt("CurrentReading");
+            
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        
+        return 0;
+    }
+    
+    
+    private static String getDateOfLastBill(String meterCode){
+        
+        try(
+            Connection connect = DbConnection.connect();
+            PreparedStatement ps = connect.prepareStatement("select DateOfBill from OldCustomer "
+                    + "where MeterCode = ? order by Num DESC limit 1");
+        ){
+            
+            ps.setString(1, meterCode);
+            ResultSet r = ps.executeQuery();
+            
+            return r.getString("DateOfBill");
+            
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        
+        return "";
+    }
+    
+    
+    private static String generateNewDateOfBill(String latestDate){
+        
+        int months = Integer.parseInt(latestDate.substring(0, 2));
+        int years = Integer.parseInt(latestDate.substring(4, 7));
+        
+        months = (months + 1) % 12;
+        if(months == 1)
+            years++;
+        
+        return (String.format("%02d", months) + "/" + latestDate.substring(3, 4) + String.format("%03d", years));
+    }
+    
+    
+    private static String getGovernmentCodeForMeter(String meterCode){
+        
+        try(
+            Connection connect = DbConnection.connect();
+            PreparedStatement ps = connect.prepareStatement("select GovernmentCode from OldCustomer "
+                    + "where MeterCode = ? order by Num DESC limit 1");
+        ){
+            
+            ps.setString(1, meterCode);
+            ResultSet r = ps.executeQuery();
+            r.next();
+            
+            return r.getString("GovernmentCode");
+            
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        
+        return "";
+    }
+    
+
 }
