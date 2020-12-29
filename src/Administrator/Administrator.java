baea18ff3e17321ca3e97cb60ea4bbb44ff94c64
Acro146 <60201913+Acro146@users.noreply.github.com>
2020-12-29 21:07:49 +0200

    y

--- src/Administrator/Administrator.java
+++ src/Administrator/Administrator.java
@@ -1,6 +1,6 @@
 package Administrator;
 import Person.PersonDriver;
-
+import Datebase.*;
 public class Administrator extends PersonDriver {
 
     // Data fields
@@ -14,6 +14,33 @@ public class Administrator extends PersonDriver {
 
         this.administratorID = String.format("%03d", Integer.parseInt(governmentCode)) + nationalIdNum.charAt(7) + nationalIdNum.charAt(8) +
                                String.format("%04d", random) + nationalIdNum.charAt(13);
+    }
+
+    public String getAdministratorID() {
+        return administratorID;
+    }
+
+    public String getAdministratorPass() {
+        return administratorPass;
+    }
+
+    public void setAdministratorPass(String administratorPass) {
+        this.administratorPass = administratorPass;
+    }
+
+    public String getAdministratorRole() {
+        return administratorRole;
+    }
+
+    public void setAdministratorRole(String administratorRole) {
+        this.administratorRole = administratorRole;
+    }
+    
+    protected void pushAllNewAdministratorInfoToDB(){
+        AdministratorDatabase.addAdministrator(
+                getName(), getNationalIdNum(), getAddress(), getEmail(),getPhoneNumber(), getGender(),
+                getDateOfBirth(), getAdministratorID(), getAdministratorPass(), getContractDate(), getAdministratorRole()
+        );
     }
     
 }
